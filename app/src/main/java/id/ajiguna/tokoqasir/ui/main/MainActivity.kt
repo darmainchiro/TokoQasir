ad027462efcf3117977f55ba86c07600fb9e0abb
Aji Guna <gunaitsugo@gmail.com>
2022-10-09 19:11:58 +0700

    starter project

---
+++ app/src/main/java/id/ajiguna/tokoqasir/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ajiguna.tokoqasir.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ajiguna.tokoqasir.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
